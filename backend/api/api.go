8f2f11dacdecc1799567a1a9dc58c3528be554d7
haiderali077 <246067.haider@gmail.com>
2026-01-05 14:15:00 +0500

    define totalfields struct for all platform fields

--- backend/api/api.go
+++ backend/api/api.go
@@ -10,11 +10,38 @@ type Error struct {
 	Message string
 }
 
+type TotalFields struct {
+	Platforms     []string `json:"platforms"`
+	Title         string   `json:"title"`
+	Description   string   `json:"description"`
+	Caption       string   `json:"caption"`
+	MediaFile     string   `json:"media_file"`
+	PrivacyStatus string   `json:"privacy_status"`
+	CategoryID    string   `json:"category_id"`
+	Tags          []string `json:"tags"`
+	ImageURL      string   `json:"image_url"`
+	LocationID    string   `json:"location_id"`
+	UserTags      string   `json:"user_tags"`
+	BoardID       string   `json:"board_id"`
+	Link          string   `json:"link"`
+	SourceType    string   `json:"source_type"`
+	Subreddit     string   `json:"subreddit"`
+	PostType      string   `json:"post_type"`
+	Text          string   `json:"text"`
+	URL           string   `json:"url"`
+	Resubmit      bool     `json:"resubmit"`
+	NSFW          bool     `json:"nsfw"`
+	Author        string   `json:"author"`
+	LifecycleState string  `json:"lifecycle_state"`
+	TextLinkedIn  string   `json:"text_linkedin"`
+	MediaType     string   `json:"media_type"`
+	MediaStatus   string   `json:"media_status"`
+	MediaPath     string   `json:"media_path"`
+	Visibility    string   `json:"visibility"`
+}
+
 func writeError(w http.ResponseWriter, message string, code int) {
-	resp := Error{
-		Code:    code,
-		Message: message,
-	}
+	resp := Error{Code: code, Message: message}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(resp)
