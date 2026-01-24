bcc5ed71ec4306fa2771a17671a9db5b62c78e13
haiderali077 <246067.haider@gmail.com>
2026-01-24 09:30:00 +0500

    add linkedin uploader struct

--- backend/internal/tools/schema.go
+++ backend/internal/tools/schema.go
@@ -1,60 +1,35 @@
 package tools
 
 type YouTubeUploader struct {
-	AccessToken   string
-	PlatformName  string
-	Title         string
-	Description   string
-	Tags          []string
-	CategoryID    string
-	PrivacyStatus string
-	MediaFile     string
+	AccessToken   string; PlatformName  string
+	Title         string; Description   string
+	Tags          []string; CategoryID    string
+	PrivacyStatus string; MediaFile     string
 }
-
 type InstagramUploader struct {
-	AccessToken  string
-	PlatformName string
-	ImageURL     string
-	Caption      string
-	LocationID   string
-	UserTags     string
+	AccessToken  string; PlatformName string
+	ImageURL     string; Caption      string
+	LocationID   string; UserTags     string
 }
-
 type PinterestUploader struct {
-	AccessToken  string
-	PlatformName string
-	BoardID      string
-	Title        string
-	Description  string
-	Link         string
-	SourceType   string
-	ImageURL     string
+	AccessToken  string; PlatformName string
+	BoardID      string; Title        string
+	Description  string; Link         string
+	SourceType   string; ImageURL     string
 }
-
 type RedditUploader struct {
-	AccessToken  string
-	PlatformName string
-	Subreddit    string
-	PostType     string
-	Title        string
-	Text         string
-	URL          string
-	Resubmit     bool
-	NSFW         bool
+	AccessToken  string; PlatformName string
+	Subreddit    string; PostType     string
+	Title        string; Text         string
+	URL          string; Resubmit     bool; NSFW bool
 }
-
 type LinkedInUploader struct {
-	AccessToken    string
-	PlatformName   string
-	Author         string
-	LifecycleState string
-	Text           string
-	MediaType      string
-	MediaStatus    string
-	MediaPath      string
+	AccessToken    string; PlatformName   string
+	Author         string; LifecycleState string
+	Text           string; MediaType      string
+	MediaStatus    string; MediaPath      string
 	Visibility     string
 }
-
 type UploadContent interface {
 	BuildAPI() map[string]interface{}
 }
